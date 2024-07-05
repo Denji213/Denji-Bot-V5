561714f017eabf58165647ec86b23830c05fdf67
Denji213 <degnonguidi@gmail.com>
2024-07-05 14:06:42 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🥀𝐏𝐑𝐄𝐅𝐈𝐗🎯𝐍𝐀𝐑𝐔𝐓𝐎🥀: ⚔️%1⚔️\n━━━━━━━━━━━━━━━━\n🥷𝙽𝙰𝚁𝚄𝚃𝙾🌱𝚂𝚈𝚂𝚃𝙴𝙼🥷: 🫸%2🫷\n━━━━━━━━━━━━━━━━\n🏁☘️𝑀𝑌 𝐶𝑅𝐸𝐴𝑇𝑂𝑅☘️🏁\n━━━━━━━━━━━━━━━━\n🥷ʬɸʬ Sønïč❤️‍🔥Shïsûį ʬɸʬ🥷\n━━━━━━━━━━━━━━━━\n🦠𝗧𝗮𝗽𝗲 #𝗵𝗲𝗹𝗽 𝗽𝗼𝘂𝗿 𝗮𝘃𝗼𝗶𝗿 𝗹𝗮 𝗹𝗶𝘀𝘁𝗲 𝗱𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀🦠"
+			myPrefix: "🥀𝐏𝐑𝐄𝐅𝐈𝐗🎯𝗟𝘂𝘅 𝗧𝘆𝗹𝗲𝗿🥀: ⚔🥰%1⚔🥰\n━━━━━━━━━━━━━━━━\n☘️𝑀𝑌 𝐶𝑅𝐸𝐴𝑇𝑂𝑅☘️\n━━━━━━━━━━━━━━━━\n🟠𝗥𝗲𝗻𝗷𝗶 𝗦𝘁𝗮𝗿𝗳𝗮𝗹𝗹🟠\n━━━━━━━━━━━━━━━━\n✦ 𝗧𝗮𝗽𝗲 ~𝗵𝗲𝗹𝗽 𝗽𝗼𝘂𝗿 𝗮𝘃𝗼𝗶𝗿 𝗹𝗮 𝗹𝗶𝘀𝘁𝗲 𝗱𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀✦"
 		}
 	},
 
